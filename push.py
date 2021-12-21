4b9039f95ae6be768000f15303b02907c5bd7f1d
Philipp Jost <philipp.jost@protonmail.com>
2021-12-22 00:48:09 +0900

    feat: push json event from | to phone notification

--- push.py
+++ push.py
@@ -1,13 +1,15 @@
 import os
 import sys
 import requests
+import json
 from pushbullet import Pushbullet
 
 ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
 if not ACCESS_TOKEN: sys.exit('no ACCESS_TOKEN found in environment')
 
-BASE_URL = 'https://api.pushbullet.com/v2/'
-headers = { 'Access-Token': ACCESS_TOKEN }
-
 pb = Pushbullet(ACCESS_TOKEN)
-pb.push_note("title","Body")
+
+line = sys.stdin.readline()
+event = json.loads(line)
+
+pb.push_link(event["title"],event["url"],event["body"])
